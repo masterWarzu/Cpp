1b1b0eac276c660dd80d69e9c773978542ecf8f1
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-11-06 10:57:31 +0300

    Add files via upload

---
+++ GameStats_3.0.cpp
@@ -0,0 +1,20 @@
+//Майкл Доусон - "Изучаем С++ через программирование игр"
+// Программа GameStats 2.0 демонстрирует работу с константами.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const int ALIEN_POINT = 150;
+	int aliensKilled = 10;
+	int score = aliensKilled * ALIEN_POINT;
+	cout << "score: " << score << endl;
+	enum difficulty {NOVICE, EASY, NORMAL, HARD, UNBEATABLE};
+	difficulty myDifficulty = EASY;
+	enum shipCost {FIGHTER_COST = 25, BOMBER_COST, CRUISER_COST = 50};
+	shipCost myShipCost = BOMBER_COST;
+	cout << "\nTo upgrade my ship to a Cruiser will cost " << (CRUISER_COST - myShipCost) << " Resource Points.\n";
+	return 0;
+}
