c8f0068b75fecf08c8e58fc662009cd23e82ac23
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-11-23 21:27:22 +0300

    Add files via upload

---
+++ 07_ScoreRater_2.0.cpp
@@ -0,0 +1,21 @@
+// Программа Score Rater 2.0 демонстрирует работу с сусловием else
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int score;
+	cout << "Enter your score: ";
+	cin >> score;
+	if (score >= 1000)
+	{
+		cout << "You scored 1000 or more. Impressive!\n";
+	}
+	else
+	{
+		cout << "You scored less than 1000.\n";
+	}
+	return 0;
+}
