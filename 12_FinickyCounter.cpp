6af133be4252a1e02d99dd298f26487c0cb7be23
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-11-30 11:53:09 +0300

    Add files via upload

---
+++ 12_FinickyCounter.cpp
@@ -0,0 +1,26 @@
+﻿// Программа Finicky Counter демонстрирует работу с инструкциями break и continue
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int count = 0;
+	while (true)
+	{
+		count += 1;
+		//заканчиваем цикл, если значение count превышает 10
+		if (count > 10)
+		{
+			break;
+		}
+		// пропускаем число 5
+		if (count == 5)
+		{
+			continue;
+		}
+		cout << count << endl;
+	}
+	return 0;
+}
