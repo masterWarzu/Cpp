508b36e90b5bb38be4e03a18302099ce4527bcc3
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-11-30 11:38:40 +0300

    Add files via upload

---
+++ 11_PlayAgain_2.0.cpp
@@ -0,0 +1,18 @@
+﻿// Программа Play Again 2.0 демонстрирует работу с циклами do
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	char again;
+	do
+	{
+		cout << "\n**Played an exciting game**";
+		cout << "\nDo you want to play again? (y/n): ";
+		cin >> again;
+	} while (again == 'y');
+	cout << "\nOkay, bye.";
+	return 0;
+}
