c2b6fe9e951634528781a78c84c31fe1ccebb6d5
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-12-01 14:47:21 +0300

    Add files via upload

---
+++ 14_DieRoller.cpp
@@ -0,0 +1,18 @@
+﻿// Программа Die Roller демонстрирует генериррование случайных чисел
+
+#include <iostream>
+#include <cstdlib>	// в этой бибилиотеке находится функция rand()
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(static_cast <unsigned int> (time(0)));	// функция srand() сеет генератор случайных чисел; unsigned int - зерно, которое передаётся функции srand()
+													// функция time() возвращает число в зависимости от текущих значений даты и времени
+	// запускаем генератор случайных чисел
+	int randomNumber = rand();	// генерируем случайное число
+	int die = (randomNumber % 6) + 1;	// получаем число между 1 и 6
+	cout << "You rolled a " << die << endl;
+	return 0;
+}
