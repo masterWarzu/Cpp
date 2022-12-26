193dc027ae31515d9fe6b68a41fa8d9d05971e7a
D.E.L <55325944+masterWarzu@users.noreply.github.com>
2022-12-26 20:29:28 +0300

    Add files via upload

---
+++ 17_HomeWork_1.1.cpp
@@ -0,0 +1,52 @@
+﻿// Новая версия прграммы Guess My Number, где игрок выбриает число, а компьютер должен его угадать.
+
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main()
+{
+	srand(static_cast <unsigned int> (time(0)));
+	int secretNumber;
+	int max;
+	int tries = 0;
+	int guess;
+	cout << "\tWelcome to Guess My Number v.2.0\n\n";
+	
+	do
+	{
+		cout << "Input max number: ";
+		cin >> max;
+		cout << "Input your number 1 to " << max << ": ";
+		cin >> secretNumber;
+		if (secretNumber > max)
+		{
+			cout << "\nError! The hidden number must be less than max!\n\n";
+		}
+	} while (secretNumber > max);
+
+	do
+	{
+		guess = rand() % max + 1;
+		if (secretNumber > guess)
+		{
+			++tries;
+			//cout << "\n" << tries << ") " << "To low! [ " << guess << " ]" << "\n\n";
+		}
+		else if (secretNumber < guess)
+		{
+			++tries;
+			//cout << "\n" << tries << ") " << "To high! [ " << guess << " ]" << "\n\n";
+		}
+		else
+		{
+			++tries;
+			//cout << "\n" << tries << ") " << "Bullseye!!! [ " << guess << " ]" << "\n\n";
+			cout << "\nThat's it! You guessed the number " << guess << ". Computer got it in " << tries << " guesses!\n";
+		}
+	} while (secretNumber != guess);
+
+	return 0;
+}
